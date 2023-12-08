48740782f07b2eae4ddc5e667ef987250c18963f
Gazook89 <John.W.Jones89@gmail.com>
2023-12-08 16:48:34 -0600

    add comments and some console.logs

--- scripts/mps-csv-to-json.js
+++ scripts/mps-csv-to-json.js
@@ -1,56 +1,44 @@
-// const fs = require('fs');
-// const csv = require('csv-parser');
+// Date: 2023-12-07
+// Description: This script fetches data from a specified Google Sheet using the Google Sheets API,
+// converts it to JSON format, and writes the output to a designated JSON file.
 
-// const csvFilePath = 'MPS-Properties-Full-List.csv';
-// const jsonArray = [];
+// This works with a SPECIFIC Google Sheet, containing a list of MPS properties.
+// However, the sheet and range can be changed...essentially it creates a basic array of JSON Bbjects with 
+// each header as a property name and each row it's own Object (and the row cells as the Object values).
 
-// fs.createReadStream(csvFilePath)
-//     .pipe(csv())
-//     .on('headers', (headers) => {
-//         // Assuming the first row contains column headers
-//         // Do nothing here, as we'll use headers from the 'data' event
-//     })
-//     .on('data', (row) => {
-//         const row_data = {};
-//         for (const header in row) {
-//             row_data[header] = row[header];
-//         }
-//         jsonArray.push(row_data);
-//     })
-//     .on('end', () => {
-//         fs.writeFileSync('mps-properties-output.json', JSON.stringify(jsonArray, null, 2));
-//     });
-
-
-
+// Required Libraries
 const { google } = require('googleapis');
 const fs = require('fs');
+
 // Load environment variables from .env file
 require('dotenv').config();
-
 
 // Access environment variables
 const apiKeyOrServiceAccountKeyPath = process.env.GOOGLE_SHEETS_KEY_PATH || '/absolute/path/to/your/keyfile.json';
 
-const spreadsheetId = '1s2ftbM7Bj5806vNdltHM5sIboBqBw4s-ujJB2Em6xPA';
-const range = 'Full List!A1:F75';
 // Load the service account key JSON file
 const keyPath = apiKeyOrServiceAccountKeyPath;
 const keyFile = require(keyPath);
 
-// Create an authentication client
+// Create an authentication client using the service account key
 const authClient = new google.auth.JWT({
     email: keyFile.client_email,
     key: keyFile.private_key,
     scopes: ['https://www.googleapis.com/auth/spreadsheets'],
 });
 
-// Set the authentication client in the sheets API
+// Set the authentication client for the sheets API
 const sheetsApi = google.sheets({
     version: 'v4',
     auth: authClient,
 });
 
+// Google Sheet and Output Configuration
+const spreadsheetId = '1s2ftbM7Bj5806vNdltHM5sIboBqBw4s-ujJB2Em6xPA';
+const range = 'Full List!A1:F75';
+const OUTPUT_FILE = 'data/MPS-Properties-output.json';
+
+// Fetch data from Google Sheet
 sheetsApi.spreadsheets.values.get({
     spreadsheetId,
     range,
@@ -61,11 +49,13 @@ sheetsApi.spreadsheets.values.get({
     }
 
     const rows = res.data.values;
+    console.log(`${rows.length} rows found. Converting to JSON...`);
 
     if (rows.length) {
         const headers = rows[0];
         const jsonArray = [];
 
+        // Convert rows to JSON objects
         for (let i = 1; i < rows.length; i++) {
             const row = rows[i];
             const row_data = {};
@@ -78,10 +68,8 @@ sheetsApi.spreadsheets.values.get({
         }
 
         // Write jsonArray to a JSON file
-        const jsonFilePath = 'output.json';
-        fs.writeFileSync(jsonFilePath, JSON.stringify(jsonArray, null, 2));
-
-        console.log(`JSON file created: ${jsonFilePath}`);
+        fs.writeFileSync(OUTPUT_FILE, JSON.stringify(jsonArray, null, 2));
+        console.log(`Data has been written to ${OUTPUT_FILE}`);
     } else {
         console.log('No data found.');
     }
