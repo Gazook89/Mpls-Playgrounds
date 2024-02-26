26cfb89bb49252c336ca8879a0e1d523edcdf8c6
Gazook89 <John.W.Jones89@gmail.com>
2024-02-26 14:01:29 -0600

    load geojson files by definitions in datasets variable.

--- src/main.js
+++ src/main.js
@@ -33,104 +33,122 @@ let datasets = [
         'note': 'data pulled from OSM via overpass-turbo'
     }
 ]
-map.on('load', () => {
 
-
-
-    (async () => {
+map.on('load', async () => {
+    for (const dataset of datasets) {
         try {
-            // Array of file paths
-            const filePaths = ['data/overpass-MPS-Playgrounds.json', 'data/overpass-MPRB-Playgrounds.json'];
+            const response = await fetch(dataset.path);
+            const geoJSONObject = await response.json();
 
+            geoJSONObject.properties = { ...dataset };
 
-            // Fetch all JSON files in parallel
-            const responses = await Promise.all(filePaths.map(filePath => fetch(filePath)));
-
-            // Extract JSON data from responses
-            const geoJSONObject = await Promise.all(responses.map(response => response.json()));
-
-            // Filter data based on category property
-            const filteredDataArray = geoJSONObject.map((collection, index) => {
-                if(
-                    collection.features[0] &&
-                    collection.features[0].properties &&
-                    collection.features[0].properties.hasOwnProperty('category')
-                ){
-                    return {
-                        ...collection,
-                        features: collection.features.filter(
-                            item => item.properties && (item.properties.category === 'Elementary School' || item.properties.name.includes('Elementary'))
-                        ),
-                    };                    
-                } else {
-                    return collection;
-                }
-            });
-
-
-            // Your code to place the data on the map
-            filteredDataArray.forEach(async (jsonData, index) => {
-                const sourceId = `iso-${index}`;
-                const layerId = `isoLayer-${index}`;
-                const colors = ['#0A7B83', '#F19C65', '#2AA876', '#FFD265', '#CE4D45'];
-
-                map.addSource(sourceId, {
-                    type: 'geojson',
-                    data: {
-                        type: 'FeatureCollection',
-                        features: [],
-                    },
-                });
-
-                map.addLayer(
-                    {
-                        id: layerId,
-                        type: 'fill',
-                        source: sourceId,
-                        layout: {},
-                        paint: {
-                            'fill-color': colors[index],
-                            'fill-opacity': 0.3,
-                        },
-                    },
-                    'poi-label'
-                );
-
-                const isoFeatures = await isochroneFromGeoJSON({
-                    data: jsonData.features,
-                    minutes: 10,
-                    profile: 'walking',
-                    urlBase: 'https://api.mapbox.com/isochrone/v1/mapbox/',
-                });
-
-                const isoFeatureCollection = {
-                    type: 'FeatureCollection',
-                    features: isoFeatures,
-                }
-
-                // console.log(jsonData)
-                // // Create a Blob containing the JSON data
-                // const blob = new Blob([JSON.stringify(isoFeatureCollection, null, 2)], { type: 'application/json' });
-
-                // // Create a download link
-                // const downloadLink = document.createElement('a');
-                // downloadLink.href = window.URL.createObjectURL(blob);
-                // downloadLink.download = `isoFeatureCollection-${jsonData.source_agency.short}.json`;
-
-                // // Append the link to the body
-                // document.body.appendChild(downloadLink);
-
-                // // Programmatically click the link to trigger the download
-                // downloadLink.click();
-
-                // // Remove the link from the DOM
-                // document.body.removeChild(downloadLink);
-
-
-                map.getSource(sourceId).setData(isoFeatureCollection);
-            });
+            console.log(geoJSONObject);
         } catch (error) {
             console.error('Error fetching or processing JSON:', error);
         }
-    })();
+    }
+
 });
+
+
+// map.on('load', () => {
+
+
+
+//     (async () => {
+//         try {
+//             // Array of file paths
+//             const filePaths = ['data/overpass-MPS-Playgrounds.json', 'data/overpass-MPRB-Playgrounds.json'];
+
+
+//             // Fetch all JSON files in parallel
+//             const responses = await Promise.all(filePaths.map(filePath => fetch(filePath)));
+
+//             // Extract JSON data from responses
+//             const geoJSONObject = await Promise.all(responses.map(response => response.json()));
+
+//             // Filter data based on category property
+//             const filteredDataArray = geoJSONObject.map((collection, index) => {
+//                 if(
+//                     collection.features[0] &&
+//                     collection.features[0].properties &&
+//                     collection.features[0].properties.hasOwnProperty('category')
+//                 ){
+//                     return {
+//                         ...collection,
+//                         features: collection.features.filter(
+//                             item => item.properties && (item.properties.category === 'Elementary School' || item.properties.name.includes('Elementary'))
+//                         ),
+//                     };                    
+//                 } else {
+//                     return collection;
+//                 }
+//             });
+
+
+//             // Your code to place the data on the map
+//             filteredDataArray.forEach(async (jsonData, index) => {
+//                 const sourceId = `iso-${index}`;
+//                 const layerId = `isoLayer-${index}`;
+//                 const colors = ['#0A7B83', '#F19C65', '#2AA876', '#FFD265', '#CE4D45'];
+
+//                 map.addSource(sourceId, {
+//                     type: 'geojson',
+//                     data: {
+//                         type: 'FeatureCollection',
+//                         features: [],
+//                     },
+//                 });
+
+//                 map.addLayer(
+//                     {
+//                         id: layerId,
+//                         type: 'fill',
+//                         source: sourceId,
+//                         layout: {},
+//                         paint: {
+//                             'fill-color': colors[index],
+//                             'fill-opacity': 0.3,
+//                         },
+//                     },
+//                     'poi-label'
+//                 );
+
+//                 const isoFeatures = await isochroneFromGeoJSON({
+//                     data: jsonData.features,
+//                     minutes: 10,
+//                     profile: 'walking',
+//                     urlBase: 'https://api.mapbox.com/isochrone/v1/mapbox/',
+//                 });
+
+//                 const isoFeatureCollection = {
+//                     type: 'FeatureCollection',
+//                     features: isoFeatures,
+//                 }
+
+//                 // console.log(jsonData)
+//                 // // Create a Blob containing the JSON data
+//                 // const blob = new Blob([JSON.stringify(isoFeatureCollection, null, 2)], { type: 'application/json' });
+
+//                 // // Create a download link
+//                 // const downloadLink = document.createElement('a');
+//                 // downloadLink.href = window.URL.createObjectURL(blob);
+//                 // downloadLink.download = `isoFeatureCollection-${jsonData.source_agency.short}.json`;
+
+//                 // // Append the link to the body
+//                 // document.body.appendChild(downloadLink);
+
+//                 // // Programmatically click the link to trigger the download
+//                 // downloadLink.click();
+
+//                 // // Remove the link from the DOM
+//                 // document.body.removeChild(downloadLink);
+
+
+//                 map.getSource(sourceId).setData(isoFeatureCollection);
+//             });
+//         } catch (error) {
+//             console.error('Error fetching or processing JSON:', error);
+//         }
+//     })();
+// });
